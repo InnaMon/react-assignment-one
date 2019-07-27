d2716d37fecbf94e1964f847387fc577ace7f82c
Inna Mon <inna.mon@gmail.com>
2019-07-27 16:46:39 -0500

    created two component, one with an input element and another with multiple paragraphs

---
+++ src/Components/UserOutput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const UserOutput = () => {
+    return (
+        <div>
+            <p>Hello! I am so excited about this first React.js assignment!</p>
+            <p>It may be work but it WILL be worth it!!</p>
+        </div>
+    )
+}
+
+export default UserOutput;
